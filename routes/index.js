b1fc4ab5c212a4c83df408ff4c71ee01a7ce16c2
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-27 12:00:10 +0100

    index

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController';
+
+const router = Router();
+router.get('/status', AppController.getStatus);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files/:id/data', FilesController.getFile);
+router.get('/files', FilesController.getIndex)
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+export default router;
